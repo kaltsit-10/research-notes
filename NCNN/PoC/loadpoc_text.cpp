237e22161328988bedacf5ffce2f3e64ce07c309
kaltsit-10 <3270694207@qq.com>
2026-08-29 13:51:03 +0800

    Delete NCNN directory

--- NCNN/PoC/loadpoc_text.cpp
+++
@@ -1,8 +0,0 @@
-#include "net.h"
-#include <cstdio>
-int main(int argc, char** argv) {
-    ncnn::Net net;
-    int ret = net.load_param(argv[1]);
-    printf("load_param ret=%d\n", ret);
-    return 0;
-}
